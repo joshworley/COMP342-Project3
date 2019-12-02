d0968b9c6290c720a19f87e0a04033c25b0a2667
Ethan Harvey <43649122+ethanharvey98@users.noreply.github.com>
2019-12-02 10:31:26 -0500

    Add files via upload

--- HTTPIoTClient.py
+++ HTTPIoTClient.py
@@ -1,10 +1,26 @@
 import socket
 import json
 
+# port number
+port = 80
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.17.112.74', 80))
-s.send(b'GET /sensors HTTP/1.1\r\n\r\n')
-reply = s.recv(1024)
-print(reply)
-reply = s.recv(1024)
-print(reply)
+
+# get IP Address
+ipAddress = input("Enter a host name (URL) or IP address for the FTP server: ")
+
+try:
+    # attempt to connect
+    s.connect((ipAddress, int(port)))
+
+    # list the valid actions available to the user
+    print("These are the valid options")
+    if (False):
+        s.send(b'GET /sensors HTTP/1.1\r\n\r\n')
+        reply = s.recv(1024)
+        print(reply)
+        reply = s.recv(1024)
+        print(reply)
+
+except:
+    # failed to connect
+    print("Failed to connect")
